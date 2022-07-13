808563ad9928ac9151d77916cc2c4bb55d518bd1
simonaa99 <simona.baosic@gmail.com>
2022-07-13 23:58:04 +0200

    Inicijalizacija Maven projekta

---
+++ np_project/CommonLibrary/src/main/java/rs/ac/bg/fon/np_project/commonlibrary/CommonUtility.java
@@ -0,0 +1,9 @@
+package rs.ac.bg.fon.np_project.commonlibrary;
+
+public class CommonUtility {
+
+	public static String getAppName(){
+        return "Project";
+    }
+
+}
---
+++ np_project/Server/src/main/java/rs/ac/bg/fon/np_project/server/Main.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.np_project.server;
+
+import rs.ac.bg.fon.np_project.commonlibrary.CommonUtility;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String appName = CommonUtility.getAppName();
+        System.out.println(appName);
+
+	}
+
+}
