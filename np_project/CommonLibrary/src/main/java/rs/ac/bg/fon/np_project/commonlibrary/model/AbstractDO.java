90627e93fc2035b5e5f661b95451f7b35d09d278
simonaa99 <simona.baosic@gmail.com>
2022-07-15 17:00:08 +0200

    Dodat javaDoc za apstraktnu klasu AbstractDO i klasu Game

--- np_project/CommonLibrary/src/main/java/rs/ac/bg/fon/np_project/commonlibrary/model/AbstractDO.java
+++ np_project/CommonLibrary/src/main/java/rs/ac/bg/fon/np_project/commonlibrary/model/AbstractDO.java
@@ -6,14 +6,40 @@
 package rs.ac.bg.fon.np_project.commonlibrary.model;
 
 /**
+ *Predstavlja apstraktnu klasu cije metode treba da implementiraju sve ostale domenske klase
+ *Sadrzi metode koje su neophodne za rad sa bazom 
  *
  * @author Simona
+ * @version 1.0.0
  */
 public abstract class AbstractDO {
-    
+    /**
+     * Apstraktna metoda koja vraca string svih atributa klase koja implementira ovu metodu
+     * 
+     * @return nazive atributa klase kao String
+     * */
     public abstract String getAttributeList();
+    
+    /**
+     * Apstraktna metoda koja vraca naziv klase koja implementira ovu metodu
+     * 
+     * @return naziv klase kao String
+     * */
     public abstract String getClassName();
+    
+    /**
+     * Apstraktna metoda koja vraca vrednosti svih atributa klase koja implementira ovu metodu
+     * 
+     * @return vrednosti atributa klase kao String
+     * */
     public abstract String getAttributeValues();
+    
+    /**
+     * Apstraktna metoda koja vraca uslov koji se koristi pri kreiranju
+     * upita u bazi za klasu koja implementira ovu metodu
+     * 
+     * @return uslov za upit kao String
+     * */
     public abstract String getQueryCondition();
     
 }
--- np_project/CommonLibrary/src/main/java/rs/ac/bg/fon/np_project/commonlibrary/model/Game.java
+++ np_project/CommonLibrary/src/main/java/rs/ac/bg/fon/np_project/commonlibrary/model/Game.java
@@ -8,62 +8,151 @@ package rs.ac.bg.fon.np_project.commonlibrary.model;
 import java.io.Serializable;
 
 /**
- *
+ * Predstavlja domensku klasu za drustvenu igru
+ * Sadrzi atribute: gameid, gameName, numPlayers, gameCategory,publisher i numberInStock
+ * Ovi atributi su vazni za identifikaciju same drustvene igre
+ * 
  * @author Simona
+ * @version 1.0.0
  */
 public class Game extends AbstractDO implements Serializable {
  
+	/**
+	 * Predstavlja atribut koji je primarni kljuc u okviru baze podataka
+	 * Jedinstven je za svaku igru
+	 * Vrednost atributa je Long
+	 */
     private Long gameid;
+    
+	/**
+	 * Atribut koji predstavlja naziv drustvene igre
+	 * Vrednost atributa je String
+	 * Podrazumevana vrednost je null
+	 */
     private String gameName;
+    
+	/**
+	 * Atribut koji predstavlja broj igraca koji mogu da igraju odredjenu igru
+	 * Vrednost atributa je Integer
+	 * Podrazumevana vrednost je 0
+	 */
     private Integer numPlayers;
+    
+	/**
+	 * Atribut koji predstavlja kategoriju kojoj drustvena igra pripada
+	 * Vrednost atributa se uzima iz enuma GameCategory
+	 * Podrazumevana vrednost je Porodicne_igre
+	 * @see rs.ac.bg.fon.np_project.commonlibrary.model.GameCategory
+	 */
     private GameCategory gameCategory;
+    
+	/**
+	 * Atribut koji predstavlja kreatora igre
+	 * Vrednost atributa je tipa Publisher gde ima svoj id i ime
+	 * @see rs.ac.bg.fon.np_project.commonlibrary.model.Publisher
+	 */
     private Publisher publisher;
+    
+    /**
+	 * Atribut koji predstavlja dostupne kolicine igre
+	 * Vrednost atributa je Integer
+	 * Podrazumevana vrednost je 0
+	 */
     private Integer numberInStock;
 
+    
+    /**
+     * Vraca id igre
+     * @return id igre kao Long
+	 */
     public Long getGameid() {
         return gameid;
     }
 
+    /**
+     * Postavlja novu vrednost id igre
+     * @param gameid nova vrednost atributa gameid
+	 */
     public void setGameid(Long gameid) {
         this.gameid = gameid;
     }
-
+    
+    /**
+     * Vraca naziv igre
+     * @return naziv igre kao String
+	 */
     public String getGameName() {
         return gameName;
     }
 
+    /**
+     * Postavlja novu vrednost za naziv igre
+     * @param gameName nova vrednost atributa gameName
+	 */
     public void setGameName(String gameName) {
         this.gameName = gameName;
     }
 
+    /**
+     * Vraca broj igraca koji bi trebalo da igraju igru
+     * @return broj igraca igre kao Integer
+	 */
     public Integer getNumPlayers() {
         return numPlayers;
     }
 
+    /**
+     * Postavlja novu vrednost za broj igraca koji bi trebalo da igraju igru
+     * @param numPlayers nova vrednost atributa numPlayers
+	 */
     public void setNumPlayers(Integer numPlayers) {
         this.numPlayers = numPlayers;
     }
 
+    /**
+     * Vraca kategoriju igre
+     * @return kategoriju igre kao enum GameCategory
+	 */
     public GameCategory getGameCategory() {
         return gameCategory;
     }
 
+    /**
+     * Postavlja novu vrednost za kategoriju igre
+     * @param gameCategory nova vrednost atributa gameCategory
+	 */
     public void setGameCategory(GameCategory gameCategory) {
         this.gameCategory = gameCategory;
     }
 
+    /**
+     * Vraca izdavaca igre
+     * @return izdavaca igre kao Publisher
+	 */
     public Publisher getPublisher() {
         return publisher;
     }
 
+    /**
+     * Postavlja novu vrednost za izdavaca igre
+     * @param publisher nova vrednost atributa publisher
+	 */
     public void setPublisher(Publisher publisher) {
         this.publisher = publisher;
     }
 
+    /**
+     * Vraca kolicinu dostupnih igara za svaku igru
+     * @return broj dostupnih igara kao Integer
+	 */
     public Integer getNumberInStock() {
         return numberInStock;
     }
 
+    /**
+     * Postavlja novu vrednost za kolicinu dostupnih igara
+     * @param numberInStock nova vrednost atributa numberInStock
+	 */
     public void setNumberInStock(Integer numberInStock) {
         this.numberInStock = numberInStock;
     }
